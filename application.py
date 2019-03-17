48aaff6b3d0917168567bf639455040ec9348259
henryli88 <35701404+henryli88@users.noreply.github.com>
2019-03-17 11:08:49 -0400

    Update application.py

--- application.py
+++ application.py
@@ -110,7 +110,7 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Jenkins is not working!</h1>
+    <h1>Is Jenkins working now???!</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
   </div>
   
